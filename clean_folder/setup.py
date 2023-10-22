759341fd7140cc17010cad3e1aef076c446ab396
IevgenSharnin <83iwert@gmail.com>
2023-10-22 19:51:49 +0300

    Before install package

---
+++ clean_folder/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='For clean folder',
+    version='1.0.0',
+    description='Very useful code',
+    url='http://github.com/IevgenSharnin/HomeWork7',
+    author='Ievgen Sharnin',
+    author_email='83iwert@gmail.com',
+    packages=find_namespace_packages(),
+    entry_points={'console_scripts': ['clean-folder=clean_folder.sort:sorting']})
