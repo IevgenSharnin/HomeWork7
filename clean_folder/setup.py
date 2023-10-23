4e1f2db95e4a175c1683ce7375041aa3810f3e47
IevgenSharnin <83iwert@gmail.com>
2023-10-23 20:23:50 +0300

    Final sent

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='For clean folder',
-    version='1.1.3f',
+    version='1.1.3',
     description='Very useful code, I think. It"s sorting your files in some folder',
     url='http://github.com/IevgenSharnin/HomeWork7',
     author='Ievgen Sharnin',
